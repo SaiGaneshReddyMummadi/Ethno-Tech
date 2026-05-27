438f7582f5ebb95c9cd5ea11315f1239c0e8da44
Mummadi Sai Ganesh Reddy <saiganeshreddymummadi@gmail.com>
2026-05-27 14:22:14 +0530

    Added new file

--- ElectricBill.java
+++ ElectricBill.java
@@ -1,9 +1,14 @@
-public class ElectricBill {
-    public static void main(String[] args){
-        int units = 350;
+import java.util.Scanner;
+public class ElectricBill 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int units = sc.nextInt();
         System.out.println("Electricty bill " + eleBill(units));
     }
-    static int eleBill(int units){
+    static int eleBill(int units)
+    {
         return ((units > 250) & true) ? units - 250 : 0;
     }
 }
